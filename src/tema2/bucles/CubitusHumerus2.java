4fe52dce649d1208c3cca9df43d0c7b8a53762a8
alumno01 <mromjim783@g.educaand.es>
2024-10-22 19:00:32 +0200

    a

---
+++ src/tema2/bucles/CubitusHumerus2.java
@@ -0,0 +1,12 @@
+package tema2.bucles;
+
+public class CubitusHumerus2 {
+    public static void main(String[] args) {
+
+        //Nuestros dos romanos favoritos juegan a los dados
+        //Cada uno tira dos dados
+        //Gana aquel de los dos cuya suma de sus dados sea mayor, o
+        //aquel cuyos dos dados sean iguales
+        //juega 20 rondas y dime quien gana
+    }
+}
