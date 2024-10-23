219d5c10b16f0cfe91915d0f65d9e1bed4644288
marco <mromjim783@g.educaand.es>
2024-10-23 20:58:49 +0200

    a

---
+++ src/tema2/bucles/EjercicioClase1.java
@@ -0,0 +1,30 @@
+package tema2.bucles;
+
+public class EjercicioClase1 {
+    public static void main(String[] args) {
+
+        //Dados dos numeros numero1 y numero2, dime cuantos multiplos de 5 hay entre ellos;
+
+        int numero1=120;
+        int numero2=327;
+        int contador=0;
+        int temp;
+
+        //Que pasa si numero1 es mayor que numero2
+        if (numero1>numero2) {
+            //si el numero 1 fuera mayor habria que intercambiar los numeros
+            temp = numero1;
+            numero1 = numero2;
+            numero2 = temp;
+        }
+
+        for (int i=numero1; i<=numero2; i++ ) {
+            if (i % 5 == 0) {
+                contador++;
+
+            }
+        }
+
+        System.out.println("Múltiplos de 5 hay: " + contador);
+    }
+}
---
+++ src/tema2/bucles/EjercicioClase2.java
@@ -0,0 +1,31 @@
+package tema2.bucles;
+
+public class EjercicioClase2 {
+    public static void main(String[] args) {
+        //Tenemos que pintar la siguiente figura
+        // 10000000
+        // 01000000
+        // 00100000
+        // 00010000
+        // 00001000
+        // 00000100
+        // 00000010
+        // 00000001
+
+        //Un for dentro de otro for
+        //El for con i lleva las filas
+        //EL for con j lleva las columnas
+
+        for (int i=0; i<8; i++){
+
+            for (int j=0; j<8; j++){
+                if (i==j){
+                    System.out.print(1);
+                }else System.out.print(0);  //Cuando dejamos print sin el ln no salta a la siguiente linea
+            }
+            System.out.println(); //Usamos este println para que baje a la siguiente linea
+        }
+
+
+    }
+}
