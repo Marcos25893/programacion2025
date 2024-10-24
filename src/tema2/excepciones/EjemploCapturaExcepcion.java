c0461f8b2cba799db9b290472b9533743a522029
alumno01 <mromjim783@g.educaand.es>
2024-10-24 19:06:28 +0200

    a

---
+++ src/tema2/excepciones/EjemploCapturaExcepcion.java
@@ -0,0 +1,21 @@
+package tema2.excepciones;
+
+import java.util.Scanner;
+
+public class EjemploCapturaExcepcion {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime un numero");
+
+        //Controlar errores
+        try{
+            int numero= Integer.parseInt(sc.nextLine());
+        }catch (NumberFormatException ex){
+            System.out.println("No has introducido un numero valido");
+        }catch (Exception ex){
+            System.out.println("Otro eror");
+        }
+    }
+}
