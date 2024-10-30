21a957d2430dd3bc3c9e8c2c7e5cf032a9ea673c
marco <mromjim783@g.educaand.es>
2024-10-30 16:51:10 +0100

    a

---
+++ src/tema2/EjerciciosHoja/Ejercicio5.java
@@ -0,0 +1,22 @@
+package tema2.EjerciciosHoja;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+
+        int numero=0;
+        Scanner sc = new Scanner(System.in);
+
+
+        while (numero<1 || numero>10) {
+            try {
+                System.out.println("Ingrese un numero tiene que ser entre 1 y 10");
+                numero = Integer.parseInt(sc.nextLine());
+            }catch(Exception e) {
+                System.out.println("Error al ingresar el numero");
+            }
+        }
+
+    }
+}
