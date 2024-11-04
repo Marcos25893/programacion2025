91e720d83d2cb2f745a9dff761202b22eabc6929
alumno01 <mromjim783@g.educaand.es>
2024-11-04 18:04:25 +0100

    a

--- src/tema2/EjerciciosHoja/Ejercicio26.java
+++ src/tema2/EjerciciosHoja/Ejercicio26.java
@@ -5,35 +5,44 @@ import java.util.Scanner;
 public class Ejercicio26 {
 
     public static void convierteEnPalos(int n) {
-
+        String palos="";
+        String totalPalos="";
         int numero=0;
-         if (n>0) {
+         if (n==0){
+             System.out.print("-");
+         }else if (n>0) {
             while (n>0){
                 numero=n%10;
                 if (numero==0){
-                    System.out.println("-");
+                    palos="--";
                 }else if (numero>0){
                     for (int i=0; i<numero; i++){
-                        System.out.print("|");
+                        palos+="|";
                     }
+                    palos="-" + palos;
                 }
-                System.out.print("-");
+                if (numero==0){
+                totalPalos=palos + totalPalos;
+                }else totalPalos=palos + totalPalos;
                 n=n/10;
+                palos="";
             }
-
+             System.out.print(totalPalos);
         }
-
-
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int numero=1000;
-        while(numero>999 || numero<0) {
-            System.out.println("Dime un numero");
-            numero = Integer.parseInt(sc.nextLine());
+        int numero=-1;
+        while(numero<0) {
+            try {
+                System.out.println("Dime un numero");
+                numero = Integer.parseInt(sc.nextLine());
+            }catch (Exception ex){
+                System.out.println("Error, valor introducido no válido");
+            }
         }
         convierteEnPalos(numero);
 
