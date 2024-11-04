e08e66b5ee2da4937f378b920683b313dadc72db
marco <mromjim783@g.educaand.es>
2024-11-04 14:46:23 +0100

    a

--- src/tema2/EjerciciosHoja/Ejercicio26.java
+++ src/tema2/EjerciciosHoja/Ejercicio26.java
@@ -4,55 +4,26 @@ import java.util.Scanner;
 
 public class Ejercicio26 {
 
-    public static void convierteEnPalos(int n){
-        int resto=0;
-        int resto2=0;
-        int valor=0;
-        String cadena= String.valueOf(n);
-        if (cadena.length()==1){
-            valor=1;
-        } else if (cadena.length()==2) {
-            valor=2;
-        } else if (cadena.length()==3) {
-            valor=3;
-        }
-        for (int i=0;i<1;i++) {
-            if (valor==2) {
-                resto=n%10;
+    public static void convierteEnPalos(int n) {
+
+        int numero=0;
+         if (n>0) {
+            while (n>0){
+                numero=n%10;
+                if (numero==0){
+                    System.out.println("-");
+                }else if (numero>0){
+                    for (int i=0; i<numero; i++){
+                        System.out.print("|");
+                    }
+                }
+                System.out.print("-");
                 n=n/10;
-            } else if (valor==3) {
-                resto2=n%100;
-                resto=n%10;
-                resto2=(resto2-resto)/10;
-                n=n/100;
-            }
-            if(n==0) {
-                System.out.println("-");
-            }else {
-                for (int j = 0; j < n; j++) {
-                    System.out.print("|");
-                }
-            }
-
-            System.out.print("-");
-            if (resto2==0) {
-                System.out.print("-");
-            }else {
-                for (int j = 0; j < resto2; j++) {
-                    System.out.print("|");
-                }
-            }
-
-                System.out.print("-");
-            if (resto==0) {
-                System.out.print("-");
-            }else{
-                for (int j = 0; j < resto; j++) {
-                    System.out.print("|");
-                }
             }
 
         }
+
+
     }
 
     public static void main(String[] args) {
