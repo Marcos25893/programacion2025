1823705a5b3b8273e161e88199213a38ceb19456
marco <mromjim783@g.educaand.es>
2024-11-19 13:14:07 +0100

    a

--- src/Clases.java
+++ src/Clases.java
@@ -109,4 +109,15 @@ public class Clases {
 
     }
 
+    //Pintar un array de dos dimensiones
+
+    public static void pintar(int matriz[][]){
+        for (int i=0;i< matriz.length;i++){
+            for (int j=0;j< matriz[i].length;j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
