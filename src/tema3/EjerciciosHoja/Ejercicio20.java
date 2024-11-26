3058124f4ea516db983d848ffdedfadb4445b32d
alumno01 <mromjim783@g.educaand.es>
2024-11-26 19:13:33 +0100

    a

--- src/tema3/EjerciciosHoja/Ejercicio20.java
+++ src/tema3/EjerciciosHoja/Ejercicio20.java
@@ -13,16 +13,32 @@ public class Ejercicio20 {
         }
     }
 
+    public static boolean ganador(String matriz[][]){
+        for (int i=0;i< matriz.length;i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if(i==1 && j==1){
+                    if ((matriz[i-1][j-1].equals(matriz[i][j].equals(matriz[i+1][j+1]))) ||
+                        (matriz[i-1][j+1].equals(matriz[i][j].equals(matriz[i+1][j-1]))) ||
+                        (matriz[i-1][j].equals(matriz[i][j].equals(matriz[i+1][j])))   ||
+                        (matriz[i][j-1].equals(matriz[i][j].equals(matriz[i][j+1])))){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
         String tablero[][]=new String[3][3];
         int jugador1Fila, jugador1Columna;
         int jugador2Fila, jugador2Columna;
-        int jugadas=0;
+        boolean ganador=false;
 
         Scanner sc = new Scanner(System.in);
 
-        while(jugadas<10) {
+        while(ganador==false) {
 
             System.out.println("Jugador 1 elige fila");
             jugador1Fila = Integer.parseInt(sc.nextLine());
@@ -31,6 +47,12 @@ public class Ejercicio20 {
 
             tablero[jugador1Fila][jugador1Columna] = "X";
             pintar(tablero);
+            ganador=ganador(tablero);
+            if (ganador==true){
+                pintar(tablero);
+                System.out.println("El ganador es el jugador 1");
+                break;
+            }
 
             System.out.println("Jugador 2 elige fila");
             jugador2Fila = Integer.parseInt(sc.nextLine());
@@ -39,7 +61,12 @@ public class Ejercicio20 {
 
             tablero[jugador2Fila][jugador2Columna] = "O";
 
-            jugadas++;
+            if (ganador==true){
+                pintar(tablero);
+                System.out.println("El ganador es el jugador 2");
+                break;
+            }
+
 
             pintar(tablero);
         }
