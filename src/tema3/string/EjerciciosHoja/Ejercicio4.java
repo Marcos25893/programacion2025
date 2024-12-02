48fd5c2e457b0eded1645419f52cca654a16ead0
alumno01 <mromjim783@g.educaand.es>
2024-12-02 21:15:39 +0100

    a

--- src/tema3/string/EjerciciosHoja/Ejercicio4.java
+++ src/tema3/string/EjerciciosHoja/Ejercicio4.java
@@ -9,16 +9,32 @@ public class Ejercicio4 {
         //Por ejemplo, si se introducen las cadenas “Esta es mi amiga Ana” y “amiga” devolverá “Esta es mi
         //AMIGA Ana”.
 
-        String texto;
+        String texto="";
         String palabra;
+        int numero;
+        int tamano;
 
         Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer(texto);
 
         System.out.println("Escribe el texto");
         texto=sc.nextLine();
 
         System.out.println("Que quieres pasar a mayusculas");
         palabra= sc.nextLine();
+
+        tamano=palabra.length();
+        System.out.println(tamano);
+
+        numero=texto.indexOf(palabra);
+        System.out.println(numero);
+
+        tamano=tamano+numero;
+        sb.delete(numero, tamano);
+
+        sb.insert(numero, palabra);
+
+        System.out.println(texto);
 
 
     }
