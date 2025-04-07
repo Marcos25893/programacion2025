27c91a62abee3b107be50891eeb9a15c22c58757
alumno01 <mromjim783@g.educaand.es>
2025-04-07 18:12:17 +0200

    a

--- src/tema7/Practica1/GestionBiblioteca/Test.java
+++ src/tema7/Practica1/GestionBiblioteca/Test.java
@@ -9,15 +9,15 @@ public class Test {
         Biblioteca b1 = new Biblioteca("biblioteca", "Cuevas", "684259317","bibli@gmail.com");
 
         b1.addLibro(new Libro("1","Montaña", "Antonio","Aventura", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("2","Nubes", "Paco","Aventura", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("3","El castillo", "Ana","Aventura", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("4","El caballero", "Antonio","Aventura", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("5","La noche", "Maria","Suspense", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("6","El colegio", "Marta","Aventura", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("7","Competicion", "Marta","Aventura", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("8","La sombra del viento ", "Ana","Aventura", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("9","La princesa", "Jorge","Comedia", LocalDate.of(2015,6,21)));
-        b1.addLibro(new Libro("10","El principito", "Juan","Aventura", LocalDate.of(2015,6,21)));
+        b1.addLibro(new Libro("2","Nubes", "Paco","Aventura", LocalDate.of(2017,6,21)));
+        b1.addLibro(new Libro("3","El castillo", "Ana","Aventura", LocalDate.of(2015,12,1)));
+        b1.addLibro(new Libro("4","El caballero", "Antonio","Aventura", LocalDate.of(2018,6,2)));
+        b1.addLibro(new Libro("5","La noche", "Maria","Suspense", LocalDate.of(2013,6,21)));
+        b1.addLibro(new Libro("6","El colegio", "Marta","Romance", LocalDate.of(2011,6,21)));
+        b1.addLibro(new Libro("7","Competicion", "Marta","Aventura", LocalDate.of(2012,6,21)));
+        b1.addLibro(new Libro("8","La sombra del viento ", "Ana","Aventura", LocalDate.of(2013,6,21)));
+        b1.addLibro(new Libro("9","La princesa", "Jorge","Comedia", LocalDate.of(2012,6,15)));
+        b1.addLibro(new Libro("10","El principito", "Juan","Aventura", LocalDate.of(2013,6,20)));
 
 
 
